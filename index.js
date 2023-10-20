cf4fbadf70eae9ed0f221ffd0eed1bcf0edf81ad
mohaak47 <147770477+mohaak47@users.noreply.github.com>
2023-10-20 11:49:30 +0330

    Add files via upload

--- index.js
+++ index.js
@@ -1,53 +1,34 @@
 const express = require('express')
-const morgan = require('morgan')
 const cors = require('cors')
 const Person = requir('./models/person')
 
 const app = express()
 
-
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
-app.use(morgan('tiny'))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'));
 
-morgan.token('person', function(req, res, person) {
-    return JSON.stringify(req.body)
-})
-
-let persons = [
+let people = [
   ]
-
-const generateId = () => {
-  if (persons.length > 0 ) {
-    const maxId = Math.max(...persons.map(n => n.id))
-    return maxId + 1
-  }
-  else {
-    return 1
-  }
-}
 
 app.get('/',(request,response) => {
   response.send('<h1> Mohammad Amani </h1>')
 })
 
 app.get('/info',(request,response) => {
-  const numberOfPersons = persons.length
+  const numberOfPersons = people.length
   const date = new Date()
   response.send(`<h3>phonebook has info for ${numberOfPersons} persons </h3>`+
-      `<p> ${date} </p>`
- )
+      `<p> ${date} </p>`)
 })
 
-app.get('/api/persons',(request,response) => {
-  response.json(persons)
+app.get('/api/people',(request,response) => {
+  response.json(people)
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/people/:id', (request, response) => {
   const id = Number (request.params.id)
-  const person = persons.find( (person) => person.id === id )
+  const person = people.find( (person) => person.id === id )
   if (person) {
     response.json(person)
   }
@@ -56,29 +37,27 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
-app.delete('/api/persons/:id',(request, response) => {
+app.delete('/api/people/:id',(request, response) => {
   const id = Number(request.params.id)
-  persons = persons.filter( (person) => person.id !== id )
+  people = people.filter( (person) => person.id !== id )
   response.status(204).end()
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/people', (request, response) => {
   const body = request.body
 
-  const number = body.number
-  const name = body.name
-  if (!number || ! name) {
+  if (!body.number || !body.name) {
     return response.status(400).json({
       error: " name or number missing"
     })
   }
-  if (persons.find( (person) => person.name === name )) {
+  if (people.find( (person) => person.name === body.name )) {
        return response.status(400).json({
        error: "name must be unique"
      })
   }
 
-  const person = new Person( {
+  const person = new Person({
     name: body.name,
     number: body.number,
    })

21af71f79edef8112a65163c68670fbdf0385173
mohaak47 <147770477+mohaak47@users.noreply.github.com>
2023-10-20 13:56:43 +0330

    Add files via upload

--- models/person.js
+++ models/person.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuary', false)
+//mongoose.set('strictQuary', false)
 const url = process.env.MONGODB_URI
 
- console.log('connecting to',url)
+console.log('connecting to',url)
 
- mongoose.connect(url)
-         .then((result) => {
+mongoose.connect(url)
+        .then((result) => {
            console.log('connected to MongoDB')
          })
-         .catch((error) => {
+        .catch((error) => {
            console.log('error connecting to Mongodb', error.message)
          })
 

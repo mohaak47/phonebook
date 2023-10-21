12f7f23647967850fa730b967ae668e8c18fb475
mohaak47 <147770477+mohaak47@users.noreply.github.com>
2023-10-22 01:15:46 +0330

    Add files via upload

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const Person = require('./models/person')
 
 const app = express()
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
-app.use(express.static('dist'))
+
 
 let persons = [
   ]
